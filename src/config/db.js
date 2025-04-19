bff9b1ba6fbade64d48106f1f048997803dcbae6
Dibyendu Das <get2dibyendu@gmail.com>
2025-04-19 18:13:07 +0530

    db config changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+// Database Connection
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    console.log("url-------------",process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`✅ MongoDB connected`);
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err);
-    process.exit(1);
+    console.error("MongoDB connection error:", err);
+    process.exit(1); 
   }
 };
-
 
 module.exports = connectDB;
