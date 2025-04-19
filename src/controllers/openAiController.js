1d093a2b088f37d420490d44bf46b10c77eb080a
Manab Pratim Maity <manab2001maity@gmail.com>
2025-04-20 01:27:41 +0530

    fixes

--- src/controllers/openAiController.js
+++ src/controllers/openAiController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const openai = require('../config/openai');
 
 // ✅ Controller to handle user emotion and respond with GPT output
