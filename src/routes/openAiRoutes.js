70a47ca49e7f6cd22f1fa76df16869ae8c236320
Manab Pratim Maity <manab2001maity@gmail.com>
2025-04-20 04:19:39 +0530

    added prime middleware to rout

--- src/routes/openAiRoutes.js
+++ src/routes/openAiRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { handleUserEmotion } = require('../controllers/openAiController');
 const { verifyToken } = require('../middlewares/verifyToken');
 const { handleEnhancedChat } = require('../controllers/enhancedChatController');
+const checkPremium = require('../middlewares/checkPremium');
 
 router.post('/chat', verifyToken, handleUserEmotion);
-router.post('/enhanced-chat', verifyToken, handleEnhancedChat);
+router.post('/enhanced-chat', verifyToken, checkPremium, handleEnhancedChat);
 
 module.exports = router;
